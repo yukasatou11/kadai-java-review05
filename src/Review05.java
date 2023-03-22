b439a602721b8b6c332785bcf0c7fbfc9179a3b9
YUKA <YUKA@192.168.1.35>
2023-03-22 19:13:49 +0900

    Review05.javaの新規作成

---
+++ src/Review05.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class Review05 {
+
+    public static void main(String[] args) {
+        // 3. データベース接続と結果取得のための変数宣言
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            // 1. ドライバのクラスをJava上で読み込む
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // 2. DBと接続する
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
+                    "root",
+                    "popochan872"
+                 );
+            // 4. DBとやりとりする窓口（PreparedStatementオブジェクト）の作成
+            String sql = "SELECT * FROM person WHERE id = ?";
+            pstmt = con.prepareStatement(sql);
+
+            // 5, 6. Select文の実行と結果を格納／代入
+            System.out.print("検索キーワードを入力してください > ");
+            int input = keyIn();
+
+            // PreparedStatementオブジェクトの?に値をセット
+            pstmt.setInt(1, input);
+
+            rs = pstmt.executeQuery();
+
+            // 7. 結果を表示する
+            while( rs.next() ){
+                // Name,age列の値を取得
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                // 取得した値を表示
+                System.out.println(name);
+                System.out.println(age);
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("JDBCドライバのロードに失敗しました。");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("データベースに異常が発生しました。");
+            e.printStackTrace();
+        } finally {
+            // 8. 接続を閉じる
+            if( rs != null ){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                            System.err.println("ResultSetを閉じるときにエラーが発生しました。");
+                            e.printStackTrace();
+                }
+            }
+            if( pstmt != null ){
+                try {
+                            pstmt.close();
+                } catch (SQLException e) {
+                    System.err.println("Statementを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+                }
+            }
+            if(con != null) {
+               try {
+                   con.close();
+               } catch (SQLException e) {
+                   System.err.println("データベース切断時にエラーが発生しました。");
+                   e.printStackTrace();
+               }
+            }
+        }
+
+    }
+
+    /*
+     * キーボードから入力された値をStringで返す 引数：なし 戻り値：入力された文字列
+     */
+    private static int keyIn() {
+        String line = null;
+        try {
+            BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+            line = key.readLine();
+        } catch (IOException e) {
+
+        }
+        return Integer.parseInt(line);
+    }
+}
